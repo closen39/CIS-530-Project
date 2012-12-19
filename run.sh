c54b82c1a4c70be95a87f01674b4a8bf5407f1ca
Jason S. Mow <jason.mow@gmail.com>
2012-12-19 18:32:34 -0500

    minor workspace changes

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-for i in {0..49}
+cd /home1/c/cis530/final_project/TopicWords-v1/
+for i in {9..49}
 do
         echo Processing File $i
-        java -Xmx1000m /home1/c/cis530/final_project/TopicWords-v1/TopicSignatures configs/config$i.example
+        java -Xmx1000m TopicSignatures /home1/j/jmow/school/cis530/project/configs/config$i.example
         echo Finished processing file $i
 done
 
