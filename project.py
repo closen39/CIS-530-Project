a4685687892478542d54f9ed9738a66c9d3e77fd
Jason Mow <jason.mow@gmail.com>
2012-12-19 18:07:42 -0500

    Partial custom summarizer

--- project.py
+++ project.py
@@ -302,9 +302,10 @@ def get_pos_tags(sentences):
     tags = pos_tag(words)
     return tags
 
-def custom_summarizer(dir):
+def custom_summarizer(dir, ts_file):
     """greedily takes first and last sentence and changing some nouns/verbs"""
     files = get_all_files(dir)
+    topicwords = load_topic_words(ts_file)
     sentences = list()
     for file in files:
         temp_sents = sent_tokenize(open(file).read())
@@ -312,6 +313,40 @@ def custom_summarizer(dir):
             sentences.append(temp_sents[0])
             sentences.append(temp_sents[-1])
     tags = get_pos_tags(sentences)
+
+    vecDict = {x:get_sent_vec(x, load_collection_tokens(dir)) for x in sentences}
+    scores = dict()
+    for sent in sentences:
+        scores[sent] = 0
+        # Centrality metric
+        sim = get_sim2(sent, vecDict)
+        scores[sent] += sim
+        # topic word metric
+        words1 = [x for x in word_tokenize(sent) if x in topicwords.keys()]
+        words2 = [x for x in word_tokenize(sent) if x not in stoplist]
+        scores[sent] += float(len(words1)) / float(len(words2))
+
+    # Compile the Summary
+    sorted_sents = sorted(scores.keys(), key=lambda x: scores[x], reverse=True)
+    summary = list()
+    sumLength = 0
+    for sent in sorted_sents:
+        if valid(sent, summary, vecDict, 0.75):
+            sumLength += len(word_tokenize(sent))
+            # break if this pushes us over the threshold
+            if sumLength > 200:
+                break
+            # else, append and continue
+            summary.append(sent)
+    # construct text with sentences
+    text = ""
+    for summ in summary:
+        text += str(summ) + " "
+
+
+    return text
+
+
 
 def get_tag_mapping(map_file):
     tags = dict()
@@ -342,6 +377,40 @@ def get_random_alternative(word, context, pos):
         return choice(children).name.split('.')[0]
     else:
         return best.name.split('.')[0]
+
+def get_context(dir, words):
+    # funcwords = get_func_words('/home1/c/cis530/hw4/funcwords.txt')
+    funcwords = get_func_words('funcwords.txt')
+    sentences = load_collection_sentences(dir)
+    retDict = {word:set() for word in words}
+    for sent in sentences:
+        for word in words:
+            if word in sent and word not in funcwords:
+                context = [x for x in sent if x != word]
+                for x in context:
+                    retDict[word].add(x)
+    return retDict
+
+def get_func_words(filename):
+    retList = list()
+    f = open(filename)
+    for line in f:
+        retList.append(line.rstrip())
+    return retList
+
+def get_alternative_words(wordlist, tok_sents, pos):
+    context_dict = get_context(wordlist, tok_sents)
+    retList = list()
+    for word in wordlist:
+        alt = get_random_alternative(word, context_dict[word], pos)
+        try:
+            sim = get_lesk_similarity(word, context_dict[word], alt, context_dict[alt], pos)
+        except:
+            # Uses context of original word if alt word context not found
+            con = [x[0] for y in tok_sents for x in y]
+            sim = get_lesk_similarity(word, context_dict[word], alt, context_dict[word], pos)
+        retList.append((word, alt, sim))
+    return retList
 
 # finds and returns best Synset object
 def find_best_synset(synsets, context, pos):
